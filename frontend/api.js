46bfcaea0d55510a5821c69eb45603e9cb54d390
ThameemSyed <56760330+ThameemSyed@users.noreply.github.com>
2025-10-03 21:56:55 +1000

    Updated public IP

--- frontend/api.js
+++ frontend/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:5001/api' 
+const API_BASE_URL = 'http://13.236.117.145:5001/api' 
    
 //please work
 export default API_BASE_URL;
--- frontend/config/api.js
+++ frontend/config/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:5001/api'
+const API_BASE_URL = 'http://13.236.117.145:5001/api'
 
 
 export default API_BASE_URL;
